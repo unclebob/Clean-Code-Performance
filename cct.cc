4521dc572f9ec32a2c689120d777c9a5082f50b1
Robert C. Martin <unclebob@cleancoder.com>
2023-04-09 15:52:34 -0500

    Three derivatives and a very large array.

--- cct.cc
+++ cct.cc
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+#define N 999983
+
 using namespace std;
 
 long ms() {
@@ -11,22 +13,24 @@ long ms() {
   return (tv.tv_sec % 86400) * 1000 + (tv.tv_usec/1000);
 }
 
-static int vs[1000];
+static int vs[N];
 
-static long zero  = 0;
+static long zero = 0;
 static long one = 0;
+static long two = 0;
 
 void do_s(int x) {
   switch(x) {
     case 0: zero++; break;
     case 1: one++; break;
+	case 2: two++; break;
   }
 }
 
 
 void s_lup(int n) {
   for (int i=0; i<n; i++) {
-    do_s(vs[i%1000]);
+    do_s(vs[i%N]);
   }
 }
 
@@ -35,16 +39,21 @@ class B {
     virtual void inc() = 0;
 };
 
-static B* bs[1000];
+static B* bs[N];
 
-class D : public B {
+class D0 : public B {
   public:
     virtual void inc() {zero++;}
 };
 
-class E : public B {
+class D1 : public B {
   public:
     virtual void inc() {one++;}
+};
+
+class D2 : public B {
+  public:
+    virtual void inc() {two++;}
 };
 
 void do_c(B* b) {
@@ -53,7 +62,7 @@ void do_c(B* b) {
 
 void c_lup(int n) {
   for (int i=0; i<n; i++) {
-    do_c(bs[i%1000]);
+    do_c(bs[i%N]);
   }
 }
 
@@ -63,26 +72,36 @@ int main(int ac, char** av)
 
   long n = atol(av[1]);
 
-  for (int i=0; i<1000; i++) {
-    vs[i] = rand()%2;
-    bs[i] = rand()%2 ? (B*)new E() : (B*)new D();
+  for (int i=0; i<N; i++) {
+    vs[i] = i%3;
+	switch(i%3) {
+		case 0: bs[i] = new D0(); break;
+		case 1: bs[i] = new D1(); break;
+		case 2: bs[i] = new D2(); break;
+	}
   }
-  zero=one=0;
-	long start = ms();
+  
+  zero=one=two=0;
+  long start_s = ms();
   s_lup(n);
   long end_s = ms();
-  if (one != zero) cout<< "s-lup mismatch: " << zero << " " << one << endl;
-  zero=one=0;
+  
+  cout<< "s-lup counts: " << zero << " " << one << " " << two << endl;
+  
+  zero=one=two=0;
+  long start_c = ms();
   c_lup(n);
-  if (one != zero) cout<< "c-lup mismatch: " << zero << " " << one << endl;
   long end_c = ms();
-  int s_time = (end_s - start);
-  int c_time = (end_c - end_s);
+  
+  cout<< "c-lup counts: " << zero << " " << one << " " << two << endl;
+  
+  int s_time = (end_s - start_s);
+  int c_time = (end_c - start_c);
   int diff = (c_time - s_time);
   double s_ns = (s_time * 1000000.0) / n;
   double c_ns = (c_time * 1000000.0) / n;
   double diff_ns = (diff * 1000000.0) / n;
-  printf("switch: %d, class: %d, diff: %d;\n",s_time, c_time, diff);
-  printf("switch-ns: %g, class-ns: %g, diff-ns: %g\n", s_ns, c_ns, diff_ns);
+  
+  printf("switch-ns: %g, class-ns: %g, diff-ns: %g factor: %g\n", s_ns, c_ns, diff_ns, c_ns/s_ns);
 }
 
