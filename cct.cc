859bedd9d827fef367a53dc2c26a6c4dc893eea1
Robert C. Martin <unclebob@cleancoder.com>
2023-04-09 20:10:57 -0500

    brucewedding makes dispatch 11ns faster than switch.  How?

--- cct.cc
+++ cct.cc
@@ -1,11 +1,15 @@
-#include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
+#include <chrono>
+#include <vector>
 #include <sys/time.h>
 
-#define N 999983
-
 using namespace std;
+using namespace std::chrono;
+
+const unsigned long N = 999983L;
+static int vs[N];
+static long counters[10];
 
 long ms() {
   struct timeval tv;
@@ -13,12 +17,11 @@ long ms() {
   return (tv.tv_sec % 86400) * 1000 + (tv.tv_usec/1000);
 }
 
-static int vs[N];
 
-static long counters[10];
-
-void do_s(int x) {
-  switch(x) {
+void do_s(int x) 
+{
+    switch (x) 
+    {
     case 0: counters[0]++; break;
     case 3: counters[3]++; break;
     case 1: counters[1]++; break;
@@ -29,134 +32,103 @@ void do_s(int x) {
     case 2: counters[2]++; break;
     case 7: counters[7]++; break;
     case 8: counters[8]++; break;
-  }
+    }
 }
 
-void s_lup(int n) {
-  for (int i=0; i<n; i++) {
-    do_s(vs[i%N]);
-  }
+void s_lup(int n) 
+{
+    for (int i = 0; i < n; i++) 
+    {
+        do_s(vs[i % N]);
+    }
 }
 
-class B {
-  public:
+class B 
+{
+public:
     virtual void inc() = 0;
 };
 
 static B* bs[N];
 
-class D0 : public B {
-  public:
-    virtual void inc() {counters[0]++;}
+class D : public B 
+{
+     // int id = 0;
+
+public:
+    // D(int x) : id(x){}
+    // virtual void inc() { counters[id]++; }
+    virtual void inc() { counters[0]++; }
+	
 };
 
-class D1 : public B {
-  public:
-    virtual void inc() {counters[1]++;}
-};
+class E : public B 
+{
+     // int id = 0;
 
-class D2 : public B {
-  public:
-    virtual void inc() {counters[2]++;}
-};
-
-class D3 : public B {
-  public:
-    virtual void inc() {counters[3]++;}
-};
-
-class D4 : public B {
-  public:
-    virtual void inc() {counters[4]++;}
-};
-
-class D5 : public B {
-  public:
-    virtual void inc() {counters[5]++;}
-};
-
-class D6 : public B {
-  public:
-    virtual void inc() {counters[6]++;}
-};
-
-class D7 : public B {
-  public:
-    virtual void inc() {counters[7]++;}
-};
-
-class D8 : public B {
-  public:
-    virtual void inc() {counters[8]++;}
-};
-
-class D9 : public B {
-  public:
-    virtual void inc() {counters[9]++;}
+public:
+    // E(int x) : id(x){}
+    // virtual void inc() { counters[id]--; }
+    virtual void inc() { counters[1]++; }
+	
 };
 
 
-void do_c(B* b) {
- b->inc(); 
+
+void do_c(B* b) 
+{
+    b->inc();
 }
 
-void c_lup(int n) {
-  for (int i=0; i<n; i++) {
-    do_c(bs[i%N]);
-  }
+void c_lup(int n) 
+{
+    for (int i = 0; i < n; i++) 
+    {
+        do_c(bs[i % N]);
+    }
 }
 
 int main(int ac, char** av)
 {
-  srand(time(NULL));
+    srand(time(nullptr));
 
-  long n = atol(av[1]);
+    const auto n = atol(av[1]);
 
-  for (int i=0; i<N; i++) {
-    vs[i] = rand()%10;
-	switch(rand()%10) {
-		case 0: bs[i] = new D0(); break;
-		case 1: bs[i] = new D1(); break;
-		case 2: bs[i] = new D2(); break;
-		case 3: bs[i] = new D3(); break;
-		case 4: bs[i] = new D4(); break;
-		case 5: bs[i] = new D5(); break;
-		case 6: bs[i] = new D6(); break;
-		case 7: bs[i] = new D7(); break;
-		case 8: bs[i] = new D8(); break;
-		case 9: bs[i] = new D9(); break;	
-	}
-  }
-  
-  for (int i=0; i<10; i++)
-	  counters[i] = 0;
-  
-  long start_s = ms();
-  s_lup(n);
-  long end_s = ms();
-  
-  cout<< "s-lup counts: " << endl;
-  for (int i=0; i<10; i++)
-	  cout << counters[i] << endl;
-  
-  for (int i=0; i<10; i++)
-	  counters[i] = 0;
-  
-  long start_c = ms();
-  c_lup(n);
-  long end_c = ms();
-  
-  cout<< "c-lup counts: " << endl;
-  for (int i=0; i<10; i++)
-	  cout << counters[i] << endl;
-  
-  int s_time = (end_s - start_s);
-  int c_time = (end_c - start_c);
-  int diff = (c_time - s_time);
-  double s_ns = (s_time * 1000000.0) / n;
-  double c_ns = (c_time * 1000000.0) / n;
-  double diff_ns = (diff * 1000000.0) / n;
-  
-  printf("switch-ns: %g, class-ns: %g, diff-ns: %g factor: %g\n", s_ns, c_ns, diff_ns, c_ns/s_ns);
+    for (auto i = 0; i < N; i++) 
+    {
+        auto rnd = rand() % 10;
+        vs[i] = rnd;
+        // bs[i] = (i%2) ? (B*)new D(rnd) : (B*)new E(rnd);
+        bs[i] = (i%2) ? (B*)new D() : (B*)new E();
+		
+    }
+
+   for (int i=0; i<10; i++) counters[i] = 0;
+    
+    auto start_s = ms();
+    s_lup(n);
+    auto end_s = ms();
+
+    cout << "s-lup counts: " << endl;
+    for (const auto &count : counters)
+        cout << count << endl;
+
+    for (int i=0; i<10; i++) counters[i] = 0;
+
+    auto start_c = ms();
+    c_lup(n);
+    auto end_c = ms();
+
+    cout << "c-lup counts: " << endl;
+    for (const auto& count : counters)
+        cout << count << endl;
+
+    auto s_time = (end_s - start_s);
+    auto c_time = (end_c - start_c);
+    auto diff = (c_time - s_time);
+    auto s_ns = (s_time * 1000000.0) / n;
+    auto c_ns = (c_time * 1000000.0) / n;
+    auto diff_ns = (diff * 1000000.0) / n;
+
+    printf("switch-ns: %g, class-ns: %g, diff-ns: %g, factor: %g\n", s_ns, c_ns, diff_ns, c_ns / s_ns);
 }
-
